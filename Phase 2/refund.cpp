1a9ea5572af73cc20edc22675e971547277e715e
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2015-02-23 20:01:50 -0500

    refund: get input

--- Phase 2/refund.cpp
+++ Phase 2/refund.cpp
@@ -1,5 +1,26 @@
-//refund.cpp
-void refund(){
-	//Implement return function here
+/**
+ * refund.cpp
+ *
+ * Refunds an `x` amount from issuer's account into receiver's (buyer's) account
+ */
+void refund() {
+
+  string receiver;
+  string issuer;
+  string amount;
+
+  //Asks the user who issues the refund
+  cout << "Enter user's username who is issueing the refund: " <<endl;
+  cin >> issuer;
+
+  //Asks the user who receives the refund
+  cout << "Enter user's username who is receiving the refund: " <<endl;
+  cin >> receiver;
+
+  //Asks for the amount being refunded
+  cout << "Amount being refunded: " <<endl;
+  cin >> amount;
+
+
 	return;
 }
