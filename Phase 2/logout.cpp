0ec74fd869f7d7183e817d2ea705634c2825a592
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-02-23 20:37:35 -0500

    Added the logout module #3

--- Phase 2/logout.cpp
+++ Phase 2/logout.cpp
@@ -1,5 +1,13 @@
 //logout.cpp
 void logout(){
-	//Implement logout function here
+	//Creates the string to be written to the transaction file
+	string daily_transaction = "00_000000000000000_00_000000000"
+
+
+	//Writes this transaction to the daily transaction file
+	ofstream daily_transaction_file;
+  	daily_transaction_file.open ("transactions.etf", ios::app);
+  	daily_transaction_file << daily_transaction+"\n";
+  	daily_transaction_file.close();
 	return;
 }
