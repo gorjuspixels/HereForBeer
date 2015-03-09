d7b480e1a8edf76cb3795a027bfd8ad839c4355a
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-03-09 05:32:36 -0400

    Added a master test script to run all tests

--- front_end/Tests/login/login_should_fail_if_transaction_without_login/test_terminal.sh
+++ front_end/Tests/login/login_should_fail_if_transaction_without_login/test_terminal.sh
@@ -5,7 +5,7 @@ NC='\033[0m' # No Color
 ./../../../a.out <input.inp >output.bto
 DIFF=$(diff -q output.bto expected_output.bto)
 if [ "$DIFF" = "" ];then
-	echo -e ${green} LOGIN FAIL IF TRANSACTION WITHOUT LOGIN: PASS ${NC}
+	echo -e ${green}LOGIN FAIL IF TRANSACTION WITHOUT LOGIN: PASS ${NC}
 else
 	echo -e ${red}LOGIN FAIL IF TRANSACTION WITHOUT LOGIN: FAIL	${NC}
 fi
--- front_end/Tests/logout/logout_accepted_when_login/test_etf.sh
+++ front_end/Tests/logout/logout_accepted_when_login/test_etf.sh
@@ -2,7 +2,9 @@
 red='\033[0;31m'
 green='\033[0;32m'
 NC='\033[0m' # No Color
-rm transactions.etf
+if [  -f transactions.etf ]; then
+    rm transactions.etf
+fi
 ./../../../a.out <input.inp >output.bto
 DIFF=$(diff -q -w transactions.etf expected_transactions.etf)
 if [ "$DIFF" = "" ];then
