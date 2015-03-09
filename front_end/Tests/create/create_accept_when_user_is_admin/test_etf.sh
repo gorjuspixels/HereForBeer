55f7ae5e3b4573ee3060703726e5ac7a44ab5904
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-03-09 05:48:58 -0400

    Added create accepted when user is admin test files

---
+++ front_end/Tests/create/create_accept_when_user_is_admin/test_etf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0m' # No Color
+if [  -f transactions.etf ]; then
+    rm transactions.etf
+fi
+./../../../a.out <input.inp >output.bto
+DIFF=$(diff -q -w transactions.etf expected_transactions.etf)
+if [ "$DIFF" = "" ];then
+	echo -e ${green}CREATE ACCEPTED WHEN USER IS ADMIN: TRANSACTION OUTPUT PASS ${NC}
+else
+	echo -e ${red}CREATE ACCEPTED WHEN USER IS ADMIN: TRANSACTION OUTPUT FAIL	${NC}
+fi
