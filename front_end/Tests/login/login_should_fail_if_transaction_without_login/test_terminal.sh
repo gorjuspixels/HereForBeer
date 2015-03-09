1fc4be99b6991b5579060d66489af65063338845
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-03-09 04:54:55 -0400

    Fixed file extensions, and clarified tests in test folder

--- front_end/Tests/login/login_should_fail_if_transaction_without_login/test_terminal.sh
+++ front_end/Tests/login/login_should_fail_if_transaction_without_login/test_terminal.sh
@@ -2,8 +2,8 @@
 red='\033[0;31m'
 green='\033[0;32m'
 NC='\033[0m' # No Color
-./../../../a.out <input.inp >output.etf
-DIFF=$(diff -q output.etf expected_output.etf)
+./../../../a.out <input.inp >output.bto
+DIFF=$(diff -q output.bto expected_output.bto)
 if [ "$DIFF" = "" ];then
 	echo -e ${green} LOGIN FAIL IF TRANSACTION WITHOUT LOGIN: PASS ${NC}
 else
--- front_end/Tests/logout/logout_accepted_when_login/test_terminal.sh
+++ front_end/Tests/logout/logout_accepted_when_login/test_terminal.sh
@@ -2,8 +2,8 @@
 red='\033[0;31m'
 green='\033[0;32m'
 NC='\033[0m' # No Color
-./../../../a.out <input.inp >output.etf
-DIFF=$(diff -q -w output.etf expected_output.etf)
+./../../../a.out <input.inp >output.bto
+DIFF=$(diff -q -w output.bto expected_output.bto)
 if [ "$DIFF" = "" ];then
 	echo -e ${green}LOGOUT ACCEPTED WHEN LOGGED IN: PASS ${NC}
 else
