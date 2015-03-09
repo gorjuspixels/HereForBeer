36420bd445867c9685fbf17f36ad9340c4f4f956
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-03-09 15:05:18 -0400

    Added test files for sell fail if ticket exceed 100 #4 Added the files, but the code currently fails the test

---
+++ front_end/Tests/sell/sell_fail_if_ticket_exceed_100/test_terminal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0m' # No Color
+./../../../a.out <input.inp >output.bto
+DIFF=$(diff -q -w output.bto expected_output.bto)
+if [ "$DIFF" = "" ];then
+	echo -e ${green}SELL FAIL IF TICKET EXCEED 100: TERMINAL OUTPUT PASS ${NC}
+else
+	echo -e ${red}SELL FAIL IF TICKET EXCEED 100: TERMINAL OUTPUT FAIL	${NC}
+fi
