b5c229dd75103bb7c8337974ba8050461f98be14
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2015-03-10 12:19:16 -0400

    refund fail if username doesnt exist: passes #4

--- front_end/refund.cpp
+++ front_end/refund.cpp
@@ -1,30 +1,47 @@
 #include <string>
 #include <iostream>
 #include "refund.h"
+#include "user_file.h"
 #include "daily_transaction.h"
 using namespace std;
 
 void refund() {
 
-  string receiver;
-  string issuer;
+  string receiverUsername;
+  string issuerUsername;
+  vector<string> receiver;
+  vector<string> issuer;
   string amount;
   const string ACTION_NAME = "refund";
 
   //Asks the user who issues the refund
   cout << "Enter user's username who is issueing the refund:" << endl;
-  cin >> issuer;
+  cin >> issuerUsername;
+
+  issuer = getUser(issuerUsername);
+  if (issuer.size() == 0) {
+    cout << "ERROR: User with specified username doesn't exist" << endl;
+    return;
+  }
 
   //Asks the user who receives the refund
   cout << "Enter user's username who is receiving the refund:" << endl;
-  cin >> receiver;
+  cin >> receiverUsername;
+
+  receiver = getUser(receiverUsername);
+  if (receiver.size() == 0) {
+    cout << "ERROR: User with specified username doesn't exist" << endl;
+    return;
+  }
 
   //Asks for the amount being refunded
   cout << "Amount being refunded:" << endl;
   cin >> amount;
 
-  string data[] = {ACTION_NAME, receiver, issuer, "9999999"};
+  string data[] = {ACTION_NAME, receiverUsername, issuerUsername, amount};
   dailyTrans(4, data);
+
+  // TODO: make changes to user file
 
   return;
 }
