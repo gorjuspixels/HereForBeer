2e81295b6ff2bd0fb103948e8783642fbc66a70a
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-03-13 13:58:19 -0400

    Added comments to Event.java

--- back_end/Event.java
+++ back_end/Event.java
@@ -1,26 +1,35 @@
+/*Event.java
+	This class will model the events read in through the event file
+*/
 import java.util.ArrayList;
 import java.util.List;
 public class Event{
 	private String title;
+	//returns the name of the event
 	public String getEventName(){
 		return this.title;
 	}
+	//returns the number of available tickets
 	public int countTickets(){
 		//dummy return
 		return 0;
 	}
+	//returns the price of the event
 	public float getPrice(){
 		//Dummy return
 		return 0.0f;
 	}
+	//saves the event
 	public void save(){
 
 	}
+	//returns a list of all the events that were on the event file
 	public static List<Event> getAllEvents(){
 		//dummy return
 		List<Event> l = new ArrayList<Event>();
 		return l;
 	}
+	//returns an event as specified through the event title
 	public static Event get(String title){
 		//dummy return
 		return new Event();
