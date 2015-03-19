0023e5157f8c8254b5d83a4b987c84f535fac393
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-03-18 21:39:09 -0400

    Added Event_holder, and modified the make file for its build instructions

---
+++ back_end/Event_Holder.java
@@ -0,0 +1,21 @@
+//Event_Holder.java
+//This class will server to hold a list of all the events
+import java.util.ArrayList;
+import java.util.List;
+public class Event_Holder{
+
+	private List<Event> events;
+
+	//Returns the list of events
+	public List<Event> get(){
+		return this.events;
+	}
+	//Outputs the list of events to a file
+	public void save(){
+		/*TODO: Iterate over the events stored in the list
+		 *      create the proper string for each
+		 *      write to the new available tickets file
+		*/
+
+	}	
+}
