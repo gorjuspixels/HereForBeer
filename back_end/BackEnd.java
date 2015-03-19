662b37c3ef2d211c545ad3b04430efb73e4a1a05
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-03-18 21:59:20 -0400

    Added some of the code for the TODO's

--- back_end/BackEnd.java
+++ back_end/BackEnd.java
@@ -6,22 +6,36 @@ public class BackEnd {
 		 *     Create a user object for each user
 		 *     Add each user to a list, and store in UserAccount_Holder class
 		*/
+		try{
+			FileReader account_fr = new FileReader("UserAccounts.txt");
+
+		}		
+		catch(FileNotFoundException e){
+			System.out.println("ERROR: UserAccounts.txt not found");
+		}
 
 
 		/*TODO: Read the old available tickets file
 		 *      Create an Event class for each event in the file
 		 *      Add each event to a list, and store in the Event_Holder class 
 		*/
+		try{
+			FileReader event_fr = new FileReader("AvailableTickets.txt");
+
+		}		
+		catch(FileNotFoundException e){
+			System.out.println("ERROR: AvailableTickets.txt not found");
+		}
 
 
 		//Reads the merged daily transaction file
 		try{
-			FileReader fr = new FileReader("transactions.etf");
+			FileReader transaction_fr = new FileReader("transactions.etf");
 
 
 			//Reads the transaction file line by line
 			try{
-				BufferedReader br = new BufferedReader(fr);
+				BufferedReader br = new BufferedReader(transaction_fr);
 
 				//Applies the transactions
 				while(br.ready()){
