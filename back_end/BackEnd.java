4935c8f17acd2ada1f66828e3d303ab652f74906
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-03-18 20:30:20 -0400

    Added the TODOs for the back end program

--- back_end/BackEnd.java
+++ back_end/BackEnd.java
@@ -1,5 +1,23 @@
 public class BackEnd {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		
+
+		//TODO:Read the merged daily transaction file
+
+
+		//TODO:Read the old current user accounts file
+
+
+		//TODO: Read the old available tickets file
+
+
+		//TODO: Line by line, apply the transactions stored in the merged daily transaction file
+
+
+		//TODO: Generate the new user accounts file
+
+
+		//TODO: Generate the new available tickets file
+
 	}
 }
