208ff36d6ba6d2358f64ce511af55ab652e9f911
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2015-04-06 22:50:36 -0400

    Event: added load/save methods

--- back_end/Event.java
+++ back_end/Event.java
@@ -1,22 +1,147 @@
 /*Event.java
 	This class will model the events read in through the event file
 */
-import java.util.ArrayList;
-import java.util.List;
-public class Event{
+public class Event {
 	private String title;
-	//returns the name of the event
+  private String sellerName;
+  private float price;
+  private int count;
+
+  public Event(String data) {
+    title = "";
+
+    for (int i = 0; i < 19; i++) {
+      title += data.charAt(i);
+    }
+    title = title.replaceAll(" ", "");
+
+    sellerName = "";
+    for (int i = 20; i < 33; i++) {
+      sellerName += data.charAt(i);
+    }
+
+    count = 0;
+    String tempCount = "";
+    for (int i = 34; i < 37; i++) {
+      tempCount += data.charAt(i);
+    }
+    tempCount = tempCount.replaceFirst("^0+(?!$)", "");
+    count = Integer.valueOf(tempCount);
+
+    price = 0f;
+    String tempPrice = "";
+    for (int i = 38; i < 44; i++) {
+      tempPrice += data.charAt(i);
+    }
+    tempPrice = tempPrice.replaceFirst("^0+(?!$)", "");
+    price = Float.valueOf(tempPrice);
+  }
+
+  public Event() {}
+
+  /**
+   * Sets event title
+   * @param name - name of the event
+   */
+  public void setEventName(String name) {
+    this.title = name;
+  }
+
+  /**
+   * Sets seller's name
+   * @param name - seller's name
+   */
+  public void setSellerName(String name) {
+    this.sellerName = name;
+  }
+
+  /**
+   * Sets event price
+   * @param price - event's price
+   */
+  public void setPrice(float price) {
+    this.price = price;
+  }
+
+  /**
+   * Sets ticket count
+   * @param count - number of available tickets
+   */
+  public void setCount(int count) {
+    this.count = count;
+  }
+
+  /**
+   * Returns the name of the event
+   * @return event name
+   */
 	public String getEventName(){
 		return this.title;
 	}
-	//returns the number of available tickets
+
+  /**
+   * Returns the number of available tickets
+   * @return number of tickets
+   */
 	public int countTickets(){
-		//dummy return
-		return 0;
+		return count;
 	}
-	//returns the price of the event
+
+  /**
+   * Returns the price of the event
+   */
 	public float getPrice(){
-		//Dummy return
-		return 0.0f;
+		return price;
 	}
+
+  /**
+   * Returns seller's username
+   * @return String seller's username
+   */
+  public String getSeller() {
+    return sellerName;
+  }
+
+  /**
+   * Converts Event object to Event writable string in format EEEEEEEEEEEEEEEEEEE_SSSSSSSSSSSSS_TTT_PPPPPP
+   * @return formatted event string
+   */
+  public String toString() {
+    String s = "";
+    String eventName = this.getEventName();
+    for (int i = 0; i < 19; i++) {
+      if (i < eventName.length()) {
+        s += eventName.charAt(i);
+      } else {
+        s += " ";
+      }
+    }
+
+    s += " ";
+    String sellerName = this.getSeller();
+    for (int i = 0; i < 13; i++) {
+      if (i < sellerName.length()) {
+        s += sellerName.charAt(i);
+      } else {
+        s += " ";
+      }
+    }
+
+    s += " ";
+    String count = Integer.toString(this.countTickets());
+    for (int i = 0; i < 3 - count.length(); i++) {
+      s += "0";
+    }
+    s += count;
+
+    s += " ";
+    String tempCredit = Float.toString(this.getPrice()).replace(".0", "");
+    int length = tempCredit.length();
+    for (int i = 0; i < 6 - length; i++) {
+      tempCredit = "0" + tempCredit;
+    }
+    s += tempCredit;
+
+    return s;
+  }
 }
--- back_end/Event_Holder.java
+++ back_end/Event_Holder.java
@@ -1,21 +1,53 @@
 //Event_Holder.java
 //This class will server to hold a list of all the events
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-public class Event_Holder{
+import java.util.Scanner;
 
-	private List<Event> events;
+public class Event_Holder {
+  private List<Event> events;
+
+  public Event_Holder() {}
+
+  public Event_Holder(String fileName) {
+    events = new ArrayList<Event>();
+    File file = new File(fileName);
+
+    try {
+      Scanner scanner = new Scanner(file);
+
+      //now read the file line by line...
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+
+        Event event = new Event(line);
+        events.add(event);
+      }
+    } catch(FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
 
 	//Returns the list of events
 	public List<Event> get(){
 		return this.events;
 	}
-	//Outputs the list of events to a file
-	public void save(){
-		/*TODO: Iterate over the events stored in the list
-		 *      create the proper string for each
-		 *      write to the new available tickets file
-		*/
 
-	}	
+
+  /**
+   * Saves data to AvailableTickets.txt file
+   * @param writer - PrintWriter to write to (usually file)
+   */
+  public void save(PrintWriter writer) {
+    for (Event event : events) {
+      if (event != null && event.getEventName() != null) {
+        writer.println(event.toString());
+      }
+    }
+
+    writer.close();
+  }
 }
